e5f6c95f0ec8dd816f1d3e026b994b90ebed938d
Artticus3516 <aaravkhatter07@gmail.com>
2025-06-14 13:54:20 +0530

    Starting project

---
+++ main.py
@@ -0,0 +1,18 @@
+import email
+import socket
+import os
+import time
+
+
+
+class user:
+    def __init__(self):
+        self.user = username
+        self.pass = password
+        self.email = email
+        self.user_limit = self_limit
+
+    def security(self,user,email,password):
+
+    def allot_space(self,allocation):
+        os.mkdir('./{}'.format(self.user)
