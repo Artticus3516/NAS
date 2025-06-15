5aaaa63410da3f9d68ff766c85be4a75337af3a7
Artticus3516 <aaravkhatter07@gmail.com>
2025-06-15 21:20:53 +0530

    CREATED USER CLASS
    the user Class is helping us to create the user and created some general
    throw_error function which will help us to throw errors
    gen_statements for succcess

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import email
 import socket
 import os
+import re
 import time
 
-
-
+def throw_error(text):
+    print('-'*len(text))
+    print(text)
+    print('-'*len(text))
+def gen_statements(text,obns):
+    print('-'*len(text))
+    print(text,'for user: {}'.format([obns.user]))
+    print('-'*len(text))
 class user:
-    def __init__(self):
+    def __init__(self,username,password,email,limit):
+        if re.fullmatch(r'^[A-Za-z0-9_]@[A-Za-z0-9]+\.(com|org|edu)$',email) :
+            throw_error("Invalid Email for user: {}".format([username,email]))
+            raise(ValueError("Invalid Email for user: {}".format([username,email])))
         self.user = username
-        self.pass = password
+        self.password = password
         self.email = email
-        self.user_limit = self_limit
-
-    def security(self,user,email,password):
-
+        self.allocation = limit
+        gen_statements("User created",self)
     def allot_space(self,allocation):
-        os.mkdir('./{}'.format(self.user)
+        os.mkdir('./Root_dir_{}'.format(self.user))
+
+
+new_user = user("Artticus","Niggarmaarda","artticus9@gmail.com",50)
